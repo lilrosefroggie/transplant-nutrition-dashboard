922d76acd1153bcd2ee970ed90bea4568a1c81bd
lilrosefroggie <rose.rana@utexas.edu>
2025-11-30 22:39:35 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/transplant-nutrition-dashboard/', // Use root path for local dev
-  // When deploying to GitHub Pages, change to: base: './'
+  base: '/transplant-nutrition-dashboard/', 
 })
